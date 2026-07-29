d884e809488e505762c2dcac463a96a220e9d00d
enriquearevalo4488 <enriquearevalo4488@gmail.com>
2026-07-29 02:38:13 +0000

    Update Decrypter project files

---
+++ KCollections/data_collection.py
@@ -0,0 +1,39 @@
+ADMINS = ["ARX0001985",
+          "ARX0002176",
+          "ARX0002294"]
+
+SIMBOLS = [
+    "!", "#", "&", "@", "!", "#", "&", "#", "&", "@"]
+
+TEXTS = [
+        "Sppbbr6ZkT","byfQTKkHK2","aRUtpMcZSv","EmkZctpTg3","P75nsGUjF3","TJmy4pZRhu","sVEzpNH4AS","zUhRhTD4mF","mC9Bq9mck5","VByWdYYeAU",
+        "BzknSsEmLd","k2fSMB6Fg7","zQpffvTLzJ","BeNsjRcD7r","NNksxSgn3p","SKHctwLa3u","FAnfkFsFq9","yZkbU3YreS","gbd5npKacL","axwyHAbk54",
+        "fWCXafdUua","byqv5NbM6F","cyJnBdQD5s","K8knPndVRs","f83pj2d6Je","d76nUHsGBM","gkH2qK96Z6","QTtPDZHVFS","A7F4mVyZkR","422WJVz7Rj",
+        "b7wnn3Cdqx","wahhVPJrft","5HFz28j6mE","9UwR7gKNqf","dAk9Xzx75r","V7GnBXuXVV","3QAf7qXEx5","LjVtnCPuAH","eMTwAcQ8H5","s3amQ9pBkm",
+        "fxqLNmSRMg","MZFdrUESq5","gZ4Cd37p6e","av2H3wePrH","BkPbwAdzaE","fPMxcNdhau","kFMC5SxuDB","jBJj3QAC6S","FBgpY3TwKy","tU4TJ4XLSb",
+        "UAAzFNwhdQ","DpYrBF6WHJ","9ekZZvVHzK","VyJpKAAE8a","7fxd7GsRaq","74NRmsqAua","kysvaxZGa3","MPWQgVuuFM","PCbNEzsAsg","xkY7PfVCF3",
+        "rcz2FCm9zp","SWeMGb6Dwu","aMDTZ5qNKJ","azM8mFVqzU","bmX2F52Fs9","FGm4GSZZhn","k5mEXuFcUZ","Nde4FFatk4","TR9cPw8krz","th58R5WRne",
+        "PdeAmyth8w","RSKKrA3XQA","8A9xL88w7d","QcJZxKkwqB","HbZqv5RBPX","G9AC2GB8d5","Y3FmFbzvUn","qC8CawbhHU","nXrDLULC72","uXPEFDY4Y7",
+        "tHHQBTwSnf","sURhCVep2d","22cNQMBVhk","8H7Vtmx3UL","cRUCdQzWh2","3U8f8a7sMa","cz7XfSWTVC","JvhP4wXbUb","e3vrC9hCgP","PeLqwwzG5v",
+        "dhZstuDnUG","RmCAVjuGTH","sSe54d8A9H","5jKk2grjre","x9STh7pYV5","9V6vThpPDF","3TL6e5f2sj","Fta9ND5uEu","Nkm5xAyzGD","rdAxbxYTgM",
+        "fQrGk4UWJz","YWXUkwbdxL","MWkcUMa6xN","X78LJRGs3X","A72MNxqHUc","ydY9LAZPBT","kmsYneDbLk","SNpT4mdufR","c3YvnvMnsy","DYSthhYLYb",
+        "U3FDZ2AE6t","ELuTLWPZaD","dTcS7gFAjS","beFhjq6ZVH","EJELdWtkJV","DHYXDzQRtt","ZQt3AbxhGa","ey9Dxt4xgM","LdGyRKyQDF","vNRa3tGjHB",
+        "S7xPFUMLXH","NwBcPKb676","GRJ2F3xV8J","ruGSfaWUQA","4aBEJ72K6c","bFWAdG36wt","rvpwCGNXGg","YERjzxRnuj","P78ugvU7mW","pxStXsqXtC",
+        "N688uXd5hP","N7aBa4kZjt","BKGepTQd2Q","r5MyaPK9Wm","ZMZZLSzbYm","7eZwxkYbtv","B5Jsm4nVH7","pnActBkLGz","BHVXm6xma8","cLjD5ZFZ6h",
+        "RANU8pxV6Z","nWkQLh7zmg","ynS9cVQsHw","9qGzmA5yrC","SpSwaZkGTu","cAsZH65WYR","nzzbdGDW7m","pzQtbmyFgS","PSd2cBkeUj","f6jLsxVjW3",
+        "YHgBtU3QLE","VUyweNfXUY","4UxR6vGuZc","JHpQTYbKMa","RMLwPwVgE3","AXtYfKGkpt","WErnzHbFMY","4W8zQsRAux","JzKJET4Mph","MyUhLaJREc",
+        "B499ZgbWRS","vbPxxBbwKV","yq7Ce9pW7X","LYKmPfQubw","vXSAewJaaT","HnwLgdrym7","VGwdscL3zC","rnRbYwZrSY","KrXZzQ2bcx","8Qx86PwPvq",
+        "JhnPh9ghCb","AmmZ9K9AXr","Xd6djMvxd5","A9Zt62CTvX","vkmBNqxNW6","P3pJ4GRCe7","nczsrtqhz9","2bnagEx489","Ee7HhaGh8b","pgEXnPbg2n",
+        "eLpZ5qa5EK","R3NWeVDpW9","x2QtHXhgWb","pDBqcdca7y","X2vYN3fJXL","hJPNHvrxFU","zW3sv8Ej9a","tyse6BxzdB","GJmZPU6jGV","7gPLaqwrFC",
+        "uW5WtKBq3B","emQpAH4pp7","eKpaHEw3dt","GdPaBpKf8R","neemvrkhcy","C9mdQqvfHX","Sw8cGHaDVc","8wDUMsru9m","senvS4kqyj","xjncfSvWda",
+        "FxaXacSsF8","pY8BxDgWGw","xY9NBr5m4x","KKFQ4PCDzU","BDV8b5RUAs","aJAn7gV8Er","JtyTuTXAVq","DQWnEP3Utt","yaFhGA3EYG","hqPeRXLth8",
+        "KV28zA7FD9","WZePB3ZU7X","b89S7EnCt8","Yyk8ts9342","3z9MJhvqxT","9Y7hzWsgfT","JfZKk9cKX2","HREtUdp3R4","gYfkSCbwm5","Nfz5qYXPVe",
+        "uTTjwQGPx6","dAC97SnXvu","rHvCmjaHES","LfTavdHAHd","E3NXgSTWC6","xgksEYvyNU","6RVfDd6WHa","BYw8FjC8U7","8yHMe4QRZw","4NSWqGLHKq",
+        "PQJ8DUVfnd","yKy6kThq3B","d2Wue3eZ3v","WwwCQZH822","4fbqM4BG3V","JCQfU4rgsD","7sBN8BCHya","LhKzjK6msX","p7DZaPbWgV","YnKYEPbyKX",
+        "Ch2fJZMjfj","6SSfXATdEq","7xnZcW69tM","PnDyMbL2YU","fRNUHHn9Kn","LYZJ89Rw9P","UfdgypJb7a","KhSMTqUhNn","sqDeRHgFsk","Gkqex3KYJS",
+        "WGALarsZZ5","nqGqhVYEUj","LQNcUrMA8F","VEvTDuFXr9","CRuVtM2NEF","eFmbPAb3dc","sQzfEu7QHn","zqJr7EWsGA","nDMAkp3kRD","kgrrVef4Un",
+        "2A2kU2u7WD","uH7RkSBfBM","rfV5n5wYG4","xE3Y6R72jp","jkH8eBh9uW","B4aun9Axjh","YF6mC5Q4kq","nHwSEtvCzu","ZFq7dQB47X","87XrG6Vm68",
+        "MkCLQkQyNC","L3pvNLUKKH","4rdByKNmh8","2BmLGYrhXF","qFWxyYvKUh","NtJvNVY9ZG","VqKsvUJU6A","869CpAvWV9","QyYby3tqc9","rLufGDrhjN",
+        "Kr2DSNS5U2","vf2TcEmeNT","synDVQdVXU","3QAGPVPnNR","tUyhPSkExS","KGe3HLHLVQ","4jmRDdzaHK","bBQcm5EeQ6","XxUNKGurVx","FRcxbyGCWB",
+        "edw7ENP6Zw","bH3T3cPS5F","dFQDyaHWuP","wUCNJfth47","4wSUAq9u4z","bhcGArxrzQ","nB6F8u2HZ4","u3N6jSRWKR","XUTAzhTRwX","QNuqCaT7NX"
+    ]
---
+++ KCollections/style_collection.py
@@ -0,0 +1,15 @@
+THEME = {
+    "bg": "#1A1A2E",
+    "entry_bg": "#0F3460",
+    "accent": "#9A40D6",
+    "text": "#E0E1DD",
+    "btn_bg": "#16213E",
+    "btn_active": "#4CC9F0"
+}
+
+FONTS = {
+    "title": ("Segoe UI", 24, "bold"),
+    "label": ("Segoe UI", 9, "bold"),
+    "data": ("Consolas", 11),
+    "btn": ("Segoe UI", 10, "bold")
+}
